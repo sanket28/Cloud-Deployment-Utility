61921d909672227548d1e1c520c319867f0051c0
sank28 <ssd20072@gmail.com>
2016-03-13 16:54:49 -0400

    Added support for mqtt client

--- cloudmanager/manager/views.py
+++ cloudmanager/manager/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from subprocess import call
 
 def index(request):
 	hypervisors = ["VMWare vSphere", "Xen Server", "Microsoft Hyper-V"]
@@ -10,4 +11,17 @@ def index(request):
 		'hypervisors': hypervisors,
 		'cloudStacks': cloudStacks
 	}
+
+	return HttpResponse(template.render(context, request))
+
+def status(request):
+	template = loader.get_template('manager/status.html')
+	context = {
+		'test': 'cool'
+	}
+
+	if (request.POST):
+		print request.POST.get('subnet')
+		print call(["ls", "-l"])
+	
 	return HttpResponse(template.render(context, request))
